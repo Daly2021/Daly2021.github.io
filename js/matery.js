2121d24708abf25e65caa946afc07e158a2588b7
ck_cheng <2574833532@qq.com>
2021-04-05 23:16:38 +0800

    Site updated: 2021-04-05 23:16:37

--- js/matery.js
+++ js/matery.js
@@ -216,19 +216,4 @@ $('#fullscreen_li ').on('click', function() {
     b ? enterfullscreen() : exitfullscreen();
 })
 
-// 深色模式设置
-function switchNightMode() {
-    var body = document.body;
-    if (body.classList.contains('dark')) {
-        document.body.classList.remove('dark');
-        localStorage.setItem('dark', '0');
-        $('#nightMode').removeClass("fa-lightbulb").addClass("fa-moon-o");
-        return;
-    } else {
-        document.body.classList.add('dark');
-        localStorage.setItem('dark', '1');
-        $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb");
-        return;
-    }
-}
 
